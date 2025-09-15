f866b01aa30c6383176d45958cc1861af341a7fe
Omar-Abdelmoteleb <omar.abdelmotelbbb@gmail.com>
2025-09-15 22:37:40 +0300

    Add files via upload

---
+++ magic 8 ball code.py
@@ -0,0 +1,21 @@
+import random
+question = input("whats your question:")
+random_Number = random.randint(1,9)
+if random_Number == 1:
+   print("Yes - definitely.")
+if random_Number == 2:
+   print("It is decidedly so.")
+if random_Number == 3:
+   print("Without a doubt.")
+if random_Number == 4:
+    print("Reply hazy, try again.")
+if random_Number == 5:
+   print("Ask again later.")
+if random_Number == 6:
+   print("Better not tell you now.")
+if random_Number == 7:
+   print("My sources say no.")
+if random_Number == 8:
+   print("Outlook not so good.")
+if random_Number == 9:
+   print("Very doubtful.")
